b42bd6b31a3e34429e1dc8a0a9ec8943aa4230b7
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-06 18:23:53 -0300

    build: create folder structure and Navbar,Home,About,Skills,Projects and Contact components templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Skills from './components/Skills/Skills.jsx'
+import Projects from './components/Projects/Projects.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 }
