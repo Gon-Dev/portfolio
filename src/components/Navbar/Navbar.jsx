66a5d08cf10f8a85952b9f64afe3838c05343e1f
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-07 10:43:21 -0300

    build: add Navbar component structure and styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,44 @@
+import './Navbar.css';
+
 function Navbar(){
   return(
-    <>Navbar</>
+    <div className="Navbar-wrapper flex-row-center">
+      <div className="navbar-line-wrapper">
+        <div className="navbar-line"></div>
+        <div className="navbar-dot"></div>
+      </div>
+      <ul className="navbar-list flex-column-center">
+        <div className="navbar-name-wrapper">
+          <aside className="navbar-name">gonzalo</aside>
+          <aside className="navbar-last-name">RUIZ</aside>
+        </div>
+        <li className="navbar-item">
+          <a className="navbar-link" href="#">
+            home
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="#">
+            about
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="#">
+            skills
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="#">
+            projects
+          </a>
+        </li>
+        <li className="navbar-item">
+          <a className="navbar-link" href="#">
+            contact
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
