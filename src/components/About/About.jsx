da3671777e54c3c7034db1c110432d8f6ab7e247
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-10 19:42:18 -0300

    build: structure About component with its childrens

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,16 @@
+import './About.css';
+import Category from './Category.jsx';
+import developer from './category-developer.js';
+import guyBehind from './category-guy-behind.js';
+
 function About(){
+  
+  
   return(
-    <>About</>
+    <section className="about-wrapper flex-column-center">
+      <Category title="The developer" data={developer} />
+      <Category title="The guy behind" data={guyBehind} />
+    </section>
   )
 }
 
