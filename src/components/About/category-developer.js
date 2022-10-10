31eb7e8deb733158c51c9bc919928f8a1e1da470
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-10 19:34:20 -0300

    build: create Skills component consumable data

---
+++ src/components/About/category-developer.js
@@ -0,0 +1,25 @@
+import developerIcon from '../../assets/icons/code-json.png';
+import designerIcon from '../../assets/icons/head-thought.png';
+import teamworkIcon from '../../assets/icons/lightbulb-group.png';
+const developer = [
+  {
+    icon: developerIcon,
+    role: "Web Developer",
+    description: "I solve problems in a scalable, performing and reliable way. I dream with a world in which the web is accesible for everyone, no matter any disability.",
+    key: "developer"
+  },
+  {
+    icon: designerIcon,
+    role: "UI Designer",
+    description: "I believe the critical thinking in matter of layout, color and morphology leads to an intuitive interface and a comfortable user experience.",
+    key: "designer"
+  },
+  {
+    icon: teamworkIcon,
+    role: "Team Player",
+    description: "I’m convinced that empathy, solidarity and communication can enhance everyone’s work experience and the overall quality of the final product.",
+    key: "teamworker"
+  }
+];
+
+export default developer;
---
+++ src/components/About/category-guy-behind.js
@@ -0,0 +1,25 @@
+import roleplayerIcon from '../../assets/icons/dice-d20.png';
+import catIcon from '../../assets/icons/cat.png';
+import videogamerIcon from '../../assets/icons/controller-classic.png';
+const guyBehind = [
+  {
+    icon: roleplayerIcon,
+    role: "Roleplayer",
+    description: "I love playing tabletop RPGs like Dungeons & Dragons. It’s crazy how it pushes forward my creativity and generates infinite amounts of fun.",
+    key: "roleplayer"
+  },
+  {
+    icon: catIcon,
+    role: "Cat Lover",
+    description: "Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr. Purr.",
+    key: "cat-lover"
+  },
+  {
+    icon: videogamerIcon,
+    role: "Videogamer",
+    description: "Oh boy, they’re in my life since i had 6 years old. Amazing stories and juicy errors to fix and keep playing. I think the developer seed grown the first time there.",
+    key: "videogamer"
+  }
+];
+
+export default guyBehind;
