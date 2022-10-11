8664a26712621f50be82b5f71989e292945d02f3
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-11 20:13:21 -0300

    build: create and populate SkillsSection component with basic structure

---
+++ src/components/Skills/SkillsSection.jsx
@@ -0,0 +1,14 @@
+import Skillscard from './Skillscard.jsx';
+
+function SkillsSection({ title,data }){
+  return(
+    <div className={`skills-section-wrapper ${title.toLowerCase()}-wrapper flex-column-center`}>
+      <h1 className="skills-section-title">{title}</h1>
+      <div className="skills-cards-wrapper flex-row-center">
+        {data.map( item => <Skillscard icon={item.logo} title={item.title} key={item.key}/>)}
+      </div>
+    </div>
+  )
+}
+
+export default SkillsSection;
