b1d970f00f3a34347b17a562982344b32c5bf1dd
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-11 17:37:06 -0300

    fix: minor naming changes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,13 @@
+import technologies from './section-technologies.js';
+import SkillsSection from './SkillsSection.jsx';
+import './Skills.css';
 function Skills(){
   return(
-    <>Skills</>
+    <section className="skills-wrapper flex-column-center">
+      <div className="skills-text-band band-up">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA | GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA</div>
+      <SkillsSection title="Technologies" data={technologies} />
+      <div className="skills-text-band band-down">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA | GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA</div>
+    </section>
   )
 }
 
