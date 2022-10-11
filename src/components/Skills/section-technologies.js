ac461ebe106489a56f99accdbbdc0589dda33ad9
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-11 20:15:31 -0300

    build: create data to consume from skillsCards components in SkillsSection

---
+++ src/components/Skills/section-technologies.js
@@ -0,0 +1,28 @@
+import htmlLogo from '../../assets/logos/html.svg';
+import cssLogo from '../../assets/logos/css.svg';
+import javascriptLogo from '../../assets/logos/javascript.svg';
+import reactLogo from '../../assets/logos/react.svg';
+const technologies = [
+  {
+    title: "HTML5",
+    logo: htmlLogo,
+    key: "HTML5",
+  },
+  {
+    title: "CSS3",
+    logo: cssLogo,
+    key: "CSS3",
+  },
+  {
+    title: "JAVASCRIPT",
+    logo: javascriptLogo,
+    key: "JAVASCRIPT",
+  },
+  {
+    title: "REACT",
+    logo: reactLogo,
+    key: "REACT",
+  }
+];
+
+export default technologies;
---
+++ src/components/Skills/section-tools.js
@@ -0,0 +1,28 @@
+import figmaLogo from '../../assets/logos/figma.svg';
+import gitLogo from '../../assets/logos/git.svg';
+import viteLogo from '../../assets/logos/vite.svg';
+import vercelLogo from '../../assets/logos/vercel.svg';
+const tools = [
+  {
+    title: "FIGMA",
+    logo: figmaLogo,
+    key: "FIGMA",
+  },
+  {
+    title: "GIT",
+    logo: gitLogo,
+    key: "GIT",
+  },
+  {
+    title: "VITE",
+    logo: viteLogo,
+    key: "VITE",
+  },
+  {
+    title: "VERCEL",
+    logo: vercelLogo,
+    key: "VERCEL",
+  }
+];
+
+export default tools;
