b4e2f8f5390ac73dc192f9b1feb23c5f827d40a7
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-11 20:14:14 -0300

    build: populate Skills section component with its children components (SkillsSection and Skillscard)

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,15 @@
 import technologies from './section-technologies.js';
+import tools from './section-tools.js';
 import SkillsSection from './SkillsSection.jsx';
 import './Skills.css';
 function Skills(){
   return(
     <section className="skills-wrapper flex-column-center">
-      <div className="skills-text-band band-up">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA | GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA</div>
+      <div className="skills-text-band band-up">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA </div>
       <SkillsSection title="Technologies" data={technologies} />
-      <div className="skills-text-band band-down">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA | GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA</div>
+      <div className="skills-line"></div>
+      <SkillsSection title="Tools" data={tools} />
+      <div className="skills-text-band band-down">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA </div>
     </section>
   )
 }
