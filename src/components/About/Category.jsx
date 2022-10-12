41f4ba54a60d23f276acde4474c00b8329cb711c
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-12 16:12:17 -0300

    build: add marquee animation to Skills component

--- src/components/About/Category.jsx
+++ src/components/About/Category.jsx
@@ -1,5 +1,5 @@
 import AboutCard from './AboutCard.jsx';
-
+// VER Warning: Received `true` for a non-boolean attribute `category-title`.
 function Category( {title,data} ){
   const lowerTitleName = title.toLowerCase().split(" ").join("-");
   return(
@@ -7,7 +7,7 @@ function Category( {title,data} ){
       <div className={`category-line ${lowerTitleName}-line`}></div>
       <h1 className={`category-title ${lowerTitleName}-title`} category-title>{title}</h1>
       <div className="category-aboutcards-wrapper flex-row-center">
-        {data.map( item => <AboutCard icon={item.icon} role={item.role} description={item.description}/> )}
+        {data.map( item => <AboutCard icon={item.icon} role={item.role} description={item.description} key={item.key}/> )}
       </div>
     </aside>
   )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -5,11 +5,15 @@ import './Skills.css';
 function Skills(){
   return(
     <section className="skills-wrapper flex-column-center">
-      <div className="skills-text-band band-up">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA </div>
+      <div className="marquee-wrapper flex-row-center band-up">
+        <p className="marquee-text"> HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | HTML | CSS | JAVASCRIPT | REACT | </p>
+      </div>
       <SkillsSection title="Technologies" data={technologies} />
       <div className="skills-line"></div>
       <SkillsSection title="Tools" data={tools} />
-      <div className="skills-text-band band-down">GIT | HTML | CSS | JAVASCRIPT | REACT | FIGMA </div>
+      <div className="marquee-wrapper flex-row-center band-down">
+        <p className="marquee-text"> GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT | FIGMA | VITE | VERCEL | GIT |  </p>
+      </div>
     </section>
   )
 }
