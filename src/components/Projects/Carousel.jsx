4fafc903f0a7f7c4e6312b3a1a08ce806cbd30c0
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-16 17:30:29 -0300

    fix: fix minor spellling errors

--- src/components/Projects/Carousel.jsx
+++ src/components/Projects/Carousel.jsx
@@ -1,29 +1,38 @@
+import { useState } from 'react';
 import CarouselItem from './CarouselItem.jsx';
 import arrowNextIcon from '../../assets/symbols/arrow-next.svg'
 import arrowPrevIcon from '../../assets/symbols/arrow-prev.svg'
 function Carousel({ data }){
-  console.log(data);
+  const [currentSlide,setCurrentSlide] = useState(0);
+  const handleCarouselButtonClick = (event) => {
+    event.target.classList.contains("next") && setCurrentSlide(currentSlide => currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    event.target.classList.contains("prev") && setCurrentSlide(currentSlide => currentSlide !== 0 ? currentSlide - 1 : data.length - 1);
+  }
   return(
     <aside className="carousel-wrapper flex-row-center">
+
       <div className="carousel-buttons-wrapper flex-row-center">
-        <button className="carousel-button button-prev">
-          <img className="carousel-button-icon" src={arrowPrevIcon} alt="" />
+        <button className="carousel-button button-prev" onClick={handleCarouselButtonClick}>
+          <img className="carousel-button-icon prev" src={arrowPrevIcon} alt="" />
         </button>
-        <button className="carousel-button button-next">
-          <img className="carousel-button-icon" src={arrowNextIcon} alt="" />
+        <button className="carousel-button button-next" onClick={handleCarouselButtonClick}>
+          <img className="carousel-button-icon next" src={arrowNextIcon} alt="" />
         </button>
       </div>
-      
-      {data.map( item => 
-        <CarouselItem 
-        title={item.title} 
-        description={item.description} 
-        technologies={item.technologies} 
-          previewUrl={item.previewUrl}
-          repoUrl={item.repoUrl}
-          screens={item.screens}
+
+      <div className="carousel-inner flex-row-center" style={{transform: `translateX(${-currentSlide * 45}rem)`}}>
+        {data.map( item => 
+          <CarouselItem 
+            title={item.title} 
+            description={item.description} 
+            technologies={item.technologies} 
+            previewUrl={item.previewUrl}
+            repoUrl={item.repoUrl}
+            screens={item.screens}
+            key={item.key}
           /> 
-          )}
+        )}
+      </div>
     </aside>
   )
 }
