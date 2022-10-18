20802aea98526cc7898f517a14d8187a403b7653
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-18 18:32:13 -0300

    build: create form component for displaying content

---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,22 @@
+function ContactForm(){
+  return(
+    <div className="contact-form-wrapper flex-column-center">
+        <h3 className="form-title">
+          Thank you for coming this far!
+          <br />
+          Please, feel free to leave a comment!
+        </h3>
+      <form action="" className="form-container flex-column-center">
+        <input className="form-input" type="text" placeholder="Name"/>
+        <input className="form-input" type="email" name="" id="" placeholder="Email"/>
+        <div className="form-textarea-container">
+          <textarea className="form-description" name="" id="" placeholder="Description"></textarea>
+        </div>
+        <button className="form-submit" type="submit">Send Comment</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default ContactForm;
