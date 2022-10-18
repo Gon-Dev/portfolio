08cb705e85c35663b66ef59dc282c70f699d4c32
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-18 20:35:51 -0300

    fix: delete minor typing error in line 6

--- src/components/About/Category.jsx
+++ src/components/About/Category.jsx
@@ -1,11 +1,10 @@
 import AboutCard from './AboutCard.jsx';
-// VER Warning: Received `true` for a non-boolean attribute `category-title`.
 function Category( {title,data} ){
   const lowerTitleName = title.toLowerCase().split(" ").join("-");
   return(
     <aside className={`category-wrapper ${lowerTitleName}-wrapper flex-column-center`}>
       <div className={`category-line ${lowerTitleName}-line`}></div>
-      <h1 className={`category-title ${lowerTitleName}-title`} category-title>{title}</h1>
+      <h1 className={`category-title ${lowerTitleName}-title`}>{title}</h1>
       <div className="category-aboutcards-wrapper flex-row-center">
         {data.map( item => <AboutCard icon={item.icon} role={item.role} description={item.description} key={item.key}/> )}
       </div>
