f1fe8bc28e5a7e65380b5625032cc6ca14bd9247
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-21 19:24:51 -0300

    build: extract data from Home for social media buttons

---
+++ src/components/Home/logos.js
@@ -0,0 +1,11 @@
+import githubLogo from "../../assets/logos/github.png";
+import twitterLogo from "../../assets/logos/twitter.png";
+import linkedinLogo from "../../assets/logos/linkedin.png";
+
+const logos = [
+  {icon: githubLogo, dir: "https://github.com/Gon-Dev", key:"github"},
+  {icon: twitterLogo, dir: "https://twitter.com/goni_dev", key:"twitter"},
+  {icon: linkedinLogo, dir: "https://www.linkedin.com/in/gonzalo-ariel-ruiz/", key:"linkedin"}
+];
+
+export default logos;
