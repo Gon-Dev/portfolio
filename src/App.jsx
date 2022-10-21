6f7b5aa069aaae0b45071f1d8ce67b6e255973c6
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-21 19:28:38 -0300

    build: add context and state for activeSection handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css'
-
+import { createContext, useState } from 'react';
 import Navbar from './components/Navbar/Navbar.jsx'
 import Main from './components/Main/Main.jsx'
 
+export const sectionContext = createContext();
+
 function App() {
 
+  const [activeSection,setActiveSection] = useState(null);
+  const observerCallback = (ref) => {
+    let observerData = ref[0];
+    let observingElement = observerData.target;
+    observerData.isIntersecting ? setActiveSection(observingElement) : null;
+  }
+  let observer = new IntersectionObserver(observerCallback,{threshold: 0.6});
+
   return (
-    <div className="App flex-row-center">
-      <Navbar />
-      <Main />
-    </div>
+    <sectionContext.Provider value={{activeSection,setActiveSection,observer}}>
+      <div className="App flex-row-center">
+        <Navbar />
+        <Main />
+      </div>
+    </sectionContext.Provider>
   )
 }
 
-export default App
+export default App;
