44dc72685ac4f9b461e50a050aec226c645cb099
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-21 20:29:55 -0300

    build: import activeSection from context and set onClick event listeners to buttons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import './Navbar.css';
 import { sectionContext } from '../../App.jsx';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
+import handleClickToScroll from './handleClickToScroll.js';
+
 function Navbar(){
   let { activeSection } = useContext(sectionContext);
   let activeSectionName = activeSection ? activeSection.className.split("-")[0] : null;
@@ -16,27 +18,27 @@ function Navbar(){
           <aside className="navbar-last-name">RUIZ</aside>
         </div>
         <li className={`navbar-item ${activeSectionName === "home" ? "active" : null}`}>
-          <button className="navbar-link">
+          <button className="navbar-link" onClick={(event)=>handleClickToScroll(event)}>
             home
           </button>
         </li>
         <li className={`navbar-item ${activeSectionName === "about" ? "active" : null}`}>
-          <button className={`navbar-link`}>
+          <button className="navbar-link" onClick={(event)=>handleClickToScroll(event)}>
             about
           </button>
         </li>
         <li className={`navbar-item ${activeSectionName === "skills" ? "active" : null}`}>
-          <button className={`navbar-link`}>
+          <button className="navbar-link" onClick={(event)=>handleClickToScroll(event)}>
             skills
           </button>
         </li>
         <li className={`navbar-item ${activeSectionName === "projects" ? "active" : null}`}>
-          <button className={`navbar-link`}>
+          <button className="navbar-link" onClick={(event)=>handleClickToScroll(event)}>
             projects
           </button>
         </li>
         <li className={`navbar-item ${activeSectionName === "contact" ? "active" : null}`}>
-          <button className={`navbar-link`}>
+          <button className="navbar-link" onClick={(event)=>handleClickToScroll(event)}>
             contact
           </button>
         </li>
