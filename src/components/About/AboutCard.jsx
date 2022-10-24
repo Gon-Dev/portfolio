a23850704b98ae384ca16f95fe95e05504c0f74b
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-10-24 20:00:17 -0300

    build: change element class naming for clarity

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -1,9 +1,9 @@
 function AboutCard( { icon,role,description } ){
   return(
-    <aside className="aboutcard-wrapper flex-column-center">
-      <img className="aboutcard-icon" src={icon} alt={`${role} icon`} />
-      <h3 className="aboutcard-role">{role}</h3>
-      <p className="aboutcard-description">{description}</p>
+    <aside className="about-card-wrapper flex-column-center">
+      <img className="about-card-icon" src={icon} alt={`${role} icon`} />
+      <h3 className="about-card-role">{role}</h3>
+      <p className="about-card-description">{description}</p>
     </aside>
   )
 }
