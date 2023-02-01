863f975c8bb2af88a8406d6f641ef9546e31e2b4
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2023-02-01 19:02:49 -0300

    feature: change design for new color palette, change some writing for better UX

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,12 +2,14 @@ import './Contact.css';
 import { sectionContext } from '../../App';
 import { useContext, useEffect, useRef } from 'react';
 import ContactForm from './ContactForm';
+import Particles from '../Particles/Particles';
 function Contact(){
   const { observer } = useContext(sectionContext);
   const contactElement = useRef(null);
   useEffect(()=>{ contactElement ? observer.observe(contactElement.current) : null},[contactElement]);
   return(
     <section className="contact-wrapper flex-row-center" ref={contactElement}>
+      <Particles particlesAmount={10}/>
       <ContactForm />
     </section>
   )
