8f80c0d30515862fb65d8cb42ce4c5791bf82b10
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2023-02-01 18:50:35 -0300

    feature: add expandible funcitonality to cards and redisign with new color palette

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -1,11 +1,40 @@
-function AboutCard( { icon,role,description } ){
-  return(
-    <aside className="about-card-wrapper flex-column-center">
+import { useState } from "react";
+import expandIcon from "../../assets/symbols/expand-more.svg";
+
+function AboutCard({ icon, role, description }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <aside
+      className="about-card-wrapper flex-column-center"
+      style={{
+        maxHeight: `${isExpanded ? "20rem" : "13rem"}`,
+      }}
+    >
       <img className="about-card-icon" src={icon} alt={`${role} icon`} />
       <h3 className="about-card-role">{role}</h3>
-      <p className="about-card-description">{description}</p>
+      <p
+        className="about-card-description"
+        style={{
+          WebkitLineClamp: `${isExpanded ? "10" : "2"}`,
+        }}
+      >
+        {description}
+      </p>
+      <button
+        className="about-card-expand-button"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <img
+          className="about-card-expand-icon"
+          src={expandIcon}
+          alt="arrow down, expand icon"
+          style={{
+            transform: `${isExpanded ? "rotate(180deg)" : "rotate(0deg)"}`,
+          }}
+        />
+      </button>
     </aside>
-  )
+  );
 }
 
 export default AboutCard;
